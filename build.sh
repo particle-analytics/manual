22e25fca738c16246bfc0ab0f287ad2bb53bf04e
Carlos Labra <carlos.labra@particle-analytics.com>
2015-09-22 22:06:53 +0100

    customization

--- build.sh
+++ build.sh
@@ -8,10 +8,20 @@
 
 # dependencies for grunt:
 #   sudo npm -g install grunt-cli
+
+if [[ ! -d "node_modules" || ! -d "node_modules/grunt" ]]; then
   npm install grunt --save-dev
+fi
+
+if [ ! -d "node_modules/grunt-gitbook" ]; then
   npm install grunt-gitbook --save-dev
+fi
+if [ ! -d "node_modules/grunt-gh-pages" ]; then
   npm install grunt-gh-pages --save-dev
+fi
+if [ ! -d "node_modules/grunt-contrib-clean" ]; then
   npm install grunt-contrib-clean --save-dev
+fi
 
 gitbook init
 gitbook install
